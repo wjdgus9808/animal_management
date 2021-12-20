927372bba0c4e1d8202d97220a839376e38729f7
wjdgus9808 <38302470+wjdgus9808@users.noreply.github.com>
2021-12-21 06:49:02 +0900

    db데이터 랜더링 코드 추가

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,7 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 function Main(props){
     const isLogin = props.isLogin
+    const [inputData,setInputData]=useState([{
+        id:'',
+        date:''
+    }])
+    useEffect(async() => {
+        try{
+          const res = await axios.get('/api/crawl')
+          console.log(res);
+          const _inputData = await res.data.data.map((rowData) => ({
+                  id:rowData.id,
+                  date:rowData.date
+                })
+          )
+          setInputData(inputData.concat(_inputData))
+          console.log(inputData)
+        } catch(e){
+          console.error(e.message)
+        }
+      },[])
     const onLogout=()=>{
         sessionStorage.removeItem('user_id')
         document.location.href='/'
@@ -10,6 +29,13 @@ function Main(props){
         <div>
             <div>
                 <h2>Main 페이지</h2>
+            </div>
+            <div>
+                {inputData.map(data=>{
+                    return(<div>
+                        {data.id}    
+                    </div>)
+                })}
             </div>
             <div>
                 <button type='button' onClick={onLogout}>Logout</button>
